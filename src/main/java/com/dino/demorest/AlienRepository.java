c3a8f8fe0b72933bba84b517a9c10a9ecb78e3ce
dinob <dinob@dinobs-MacBook-Pro.local>
2020-10-11 16:35:19 -0700

    Add PUT method to update resource if exists, else create new resource in mySQL database.

--- src/main/java/com/dino/demorest/AlienRepository.java
+++ src/main/java/com/dino/demorest/AlienRepository.java
@@ -90,4 +90,20 @@ public class AlienRepository {
 			e.printStackTrace();
 		}
 	}
+	
+	public void updateAlien(Alien a) {
+		//jdbc
+		String sql = "update alien set name = ?, points = ? where id = ?";
+		try {
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, a.getName());
+			st.setInt(2, a.getPoints());
+			st.setInt(3, a.getId());
+			st.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e);
+			e.printStackTrace();
+		}
+	}
 }
--- src/main/java/com/dino/demorest/AlienResources.java
+++ src/main/java/com/dino/demorest/AlienResources.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -33,9 +35,24 @@ public class AlienResources {
 	@POST
 	@Path("alien")
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 	public Alien createAlien(Alien a) {
 		System.out.println(a);
 		repo.addAlien(a);
+		return a;
+	}
+	
+	@PUT
+	@Path("alien")
+	@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public Alien updateAlien(Alien a) {
+		System.out.println("Updating alien" + a);
+		if (repo.getAlien(a.getId()).getId() == 0) {
+			repo.addAlien(a);
+		} else {
+			repo.updateAlien(a);
+		}
+		
 		return a;
 	}
 }
